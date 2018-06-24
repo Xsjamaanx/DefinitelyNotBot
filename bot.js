a9e16e3541300575e09c5ba4c6a7e91159c39ddb
Xsjamaanx <40535682+Xsjamaanx@users.noreply.github.com>
2018-06-24 10:56:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,19 +36,6 @@ client.on('message', message => { // When the message a message is executed
 
 
 
-client.on('message', message => { // When the message a message is executed
- 
-   if(command === "say"){
-    
-  let text = args.slice(1).join(" ");
-  message.delete();
-  message.channel.send(text);
-}
-
-
-});
-
-
  
 
 // THIS  MUST  BE  THIS  WAY
