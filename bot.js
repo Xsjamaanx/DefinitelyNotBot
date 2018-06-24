78af349991c6b5efeaf8fb981a1fd1375f80ad86
Xsjamaanx <40535682+Xsjamaanx@users.noreply.github.com>
2018-06-24 10:22:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
-client.on('ready', () => {
+ 
+
+client.on('ready', () => { // know if our bot is online
+
     console.log('I am ready!');
+
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
+ 
+
+client.on('message', message => { // When the message a message is executed
+
+    if (message.content === 'ping') { // if that message matches ping
+
+       message.reply('pong'); // 
+
+       }
+
 });
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
-
-bot.user.setstatus('online')
-bot.user.setgame('IamAmazing')
+ 
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
+client.login(process.env.NDYwMzQ4MDk4NTI3NjI1MjI4.DhDc4A.oMHrs_fshq9fTbUUnk8JxlDq6P0);//where BOT_TOKEN is the token of our bo
