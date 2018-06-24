42f1113c1f6217f23aac50383c129b054a428744
Xsjamaanx <40535682+Xsjamaanx@users.noreply.github.com>
2018-06-24 10:13:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'bing,BING') {
+    if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
 });
